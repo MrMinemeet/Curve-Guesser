ecee9043b2484562bda1a6e8080eac45f6c149b3
Alexander Voglsperger <alex@wtf-my-code.works>
2023-12-10 23:57:44 +0100

    Create EdgeCollider2D based on line

--- Assets/LineController.cs
+++ Assets/LineController.cs
@@ -4,23 +4,26 @@ using UnityEngine.Serialization;
 
 public class LineController : MonoBehaviour
 {
+	private const int POINT_COUNT = 40;
+	
+	
 	// Add slider
 	[Range(0, (float) Math.PI)]
 	public float X_Value = 1.0f;
 	[SerializeField]
 	private String functionName;
 	private LineRenderer _lr;
+	private EdgeCollider2D _ec;
 	[SerializeField]
 	private Camera _camera;
 
 	private Vector3 _leftMostPos;
 	private Vector3 _rightMostPos;
-
-	private const int POINT_COUNT = 200;
 	private void Awake()
 	{
 		_lr = GetComponent<LineRenderer>();
 		_lr.positionCount = POINT_COUNT;
+		_ec = GetComponent<EdgeCollider2D>();
 		
 		_leftMostPos = _camera.ViewportToWorldPoint(new Vector3(0, 0.5f, _camera.nearClipPlane));
 		_rightMostPos = _camera.ViewportToWorldPoint(new Vector3(1, 0.5f, _camera.nearClipPlane));
@@ -31,7 +34,8 @@ public class LineController : MonoBehaviour
 	{
 		var func = Globals.Functions[functionName];
 		float width = (_rightMostPos.x - _leftMostPos.x);
-
+		var colliderPoints = new Vector2[POINT_COUNT];
+		
 
 		// Calculate and draw points for the function
 		for (int i = 0; i < POINT_COUNT; i++)
@@ -44,11 +48,13 @@ public class LineController : MonoBehaviour
 
 			// Set the position of the point
 			_lr.SetPosition(i, new Vector3(x, (float)res, 0));
-		
+			colliderPoints[i] = new Vector2(x, (float)res);
+			Debug.Log($"X: {x}, Y: {(float)res}");
 			
 			// Calculate the x value
 			//var res = func(i * X_Value);
 			//_lr.SetPosition(i, new Vector3(i, (float) res, 0));
 		}
+		_ec.points = colliderPoints;
 	}
 }
