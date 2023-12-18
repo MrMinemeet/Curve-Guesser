6ee777f36d8291224386419200f36849d871eee2
Alexander Voglsperger <alex@wtf-my-code.works>
2023-12-18 11:42:59 +0100

    Use Polycollider instead of EdgeCollider

--- Assets/LineController.cs
+++ Assets/LineController.cs
@@ -1,22 +1,25 @@
 using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class LineController : MonoBehaviour
 {
-	private const int POINT_COUNT = 1000;
+	private const int POINT_COUNT = 500;
+	private const float TOLERANCE = 0.0001f;
 	
 	
 	// Add slider
 	[Range(0, (float) Math.PI)]
 	public float X_Value = 1.0f;
+	private float _oldXValue = 1.0f;
 	[SerializeField]
 	private String functionName;
 	private LineRenderer _lr;
 	private EdgeCollider2D _ec;
-	[SerializeField]
+    private PolygonCollider2D _pc;
 	private Camera _camera;
-	private Func<float, double> _oldFunc;
+	private String _oldFuncName = "";
 
 	private readonly Vector3 _leftMostPos = new Vector3(-50f, 0f, 0f);
 	private readonly Vector3 _rightMostPos = new Vector3(50f, 0f, 0f);
@@ -25,18 +28,33 @@ public class LineController : MonoBehaviour
 		_lr = GetComponent<LineRenderer>();
 		_lr.positionCount = POINT_COUNT;
 		_ec = GetComponent<EdgeCollider2D>();
+		_pc = GetComponent<PolygonCollider2D>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if(Globals.Functions.ContainsKey(functionName))
+		// Skip if the function and X-Value didn't change
+		if (functionName == _oldFuncName && Math.Abs(_oldXValue - X_Value) < TOLERANCE)
 		{
-			this._oldFunc = Globals.Functions[functionName];
+			return;
 		}
 		
+		if (!Globals.Functions.ContainsKey(functionName))
+		{
+			Debug.Log($"Unknown function name: {functionName}");
+			return;
+		}
+		_oldXValue = X_Value;
+		_oldFuncName = functionName;
+		
+		// Get the function
+		var func = Globals.Functions[functionName];
+		
+		
+		
 		float width = (_rightMostPos.x - _leftMostPos.x);
-		var colliderPoints = new Vector2[POINT_COUNT];
+		var colliderPoints = new Vector2[POINT_COUNT + 2];
 		
 
 		// Calculate and draw points for the function
@@ -46,16 +64,18 @@ public class LineController : MonoBehaviour
 			float x = normalizedX * width + _leftMostPos.x;
 			
 			// Calculate the y value
-			var res = _oldFunc(x * X_Value);
+			var res = func(x * X_Value);
 
 			// Set the position of the point
 			_lr.SetPosition(i, new Vector3(x, (float)res  * 2, 0));
-			colliderPoints[i] = new Vector2(x, (float)res * 2);
+			colliderPoints[i + 1] = new Vector2(x, (float)res * 2);
 			
 			// Calculate the x value
 			//var res = func(i * X_Value);
 			//_lr.SetPosition(i, new Vector3(i, (float) res, 0));
 		}
-		_ec.points = colliderPoints;
+		colliderPoints[0] = new Vector2(colliderPoints.Min(v => v.x), colliderPoints.Min(v => v.y));
+		colliderPoints[POINT_COUNT + 1] = new Vector2(colliderPoints.Max(v => v.x), colliderPoints.Min(v => v.y));
+		_pc.points = colliderPoints;
 	}
 }
