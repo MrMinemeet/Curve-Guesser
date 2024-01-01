154917b20ada137b76add190ff95722ce49e665e
Alexander Voglsperger <alex@wtf-my-code.works>
2024-01-01 10:43:13 +0100

    Fix auto-thruster
    Sleigh increases thrust to a max of 8 when being between 12 and 3 o'clock.
    Otherwise the thrust is steadily reduced to 1

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -5,31 +5,34 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-	private const float MAX_THRUST = 10.0f;
+	private const float MIN_THRUST = 1.0f;
+	private const float MAX_THRUST = 8f;
 
-	[SerializeField] private float _thrust = 2.0f;
+	private float _thrust = MIN_THRUST;
 
 	// Update is called once per frame
 	void Update()
 	{
-		Debug.Log(transform.localEulerAngles.z + "\t\t" + transform.localRotation.z);
+		float eulerZ = transform.localEulerAngles.z;
+		Debug.Log($"Current rotation: {eulerZ}");
+		
 
 		// FIXME: The detection of rotation doesn't work properly yet. Because inspector rotation != transform.rotation
-		// Check if object is rotated to the bottom left corner 
-		if (transform.localEulerAngles.z > 350)
-		{
-			_thrust = 2f;
-			Debug.Log($"Reset thrust to {_thrust}");
-		}
-		// Check if object is rotated to the top right corner 
-		else if (transform.localEulerAngles.z > 20)
+		// Basically straight right is 0 and then counter clockwise the angle gets larger to 360
+		if (10 < eulerZ && eulerZ <= 90) 
 		{
 			// Add more thrust
-			_thrust = Math.Max(_thrust + (0.01f * Time.deltaTime), MAX_THRUST);
+			_thrust = Math.Min(_thrust + (5f * Time.deltaTime), MAX_THRUST);
 			Debug.Log($"Increased thrust to {_thrust}");
-
-			// Apply some on the x axis with respect to time and rotation
-			//transform.position += transform.right * _thrust * Time.deltaTime;
 		}
+		else
+		{
+			// Decrease thrust
+			_thrust = Math.Max(_thrust - (10f * Time.deltaTime), MIN_THRUST);
+			Debug.Log($"Reduced thrust to {_thrust}");
+		}
+		
+		// Apply thrust
+		transform.position += transform.right * (_thrust * Time.deltaTime);
 	}
 }
