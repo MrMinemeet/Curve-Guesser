e29b063761766d8e37393c3657a66d7d8e858c90
rechenwerk-a <adrian.vinojcic@gmail.com>
2024-01-13 15:22:06 +0100

    make graph be movable by sliders

--- Assets/Scripts/CurveController.cs
+++ Assets/Scripts/CurveController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using static Level;
+using UnityEngine.UI;
 
 public class CurveController : MonoBehaviour
 {
@@ -14,16 +16,55 @@ public class CurveController : MonoBehaviour
 
     private float start = -10f;
     private float end = 10f;
-    // Start is called before the first frame update
+
+    private List<Level> levels;
+    private int currentLevelIndex;
+
+    private Slider a;
+    private Slider b;
+    private Slider c;
+    private Slider d;
+
+    private bool isLaunched = false;
+
     void Start()
     {
         difficulty = LevelSelector.difficulty;
         function = LevelSelector.function;
         lineRenderer = GetComponent<LineRenderer>();
-        Debug.Log("Loaded");
+        levels = Level.levels.Where(level => level.difficulty == difficulty && level.function == function).ToList();
+        currentLevelIndex = 0;
+
+        a = GameObject.Find("UI/SliderGroup/Parameter A").GetComponent<Slider>();
+        b = GameObject.Find("UI/SliderGroup/Parameter B").GetComponent<Slider>();
+        c = GameObject.Find("UI/SliderGroup/Parameter C").GetComponent<Slider>();
+        d = GameObject.Find("UI/SliderGroup/Parameter D").GetComponent<Slider>();
+
+        switch (function)
+        {
+            case Function.Sine:
+                a.minValue = -5;
+                a.maxValue = 5;
+                a.value = 1;
+                b.minValue = -Mathf.PI;
+                b.maxValue = Mathf.PI;
+                b.value = 1;
+                c.minValue = -Mathf.PI;
+                c.maxValue = Mathf.PI;
+                c.value = 0;
+                d.minValue = -4;
+                d.maxValue = 4;
+                d.value = 0;
+                break;
+            case Function.Linear: 
+                break;
+            case Function.Square: 
+                break;
+            case Function.Cubic: 
+                break;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         Draw();
@@ -36,26 +77,30 @@ public class CurveController : MonoBehaviour
         {
             float progress = (float) i / (Points - 1);
             float x = Mathf.Lerp(start, end, progress);
-            float y = Mathf.Sin(x);
+            float y = Y(x);
             lineRenderer.SetPosition(i, new(x, y, 0));
         }
     }
 
     public void Launch()
     {
-        StartCoroutine(animate());
+        if (!isLaunched)
+        {
+            isLaunched = true;
+            StartCoroutine(animate());
+        }
     }
 
     IEnumerator animate() {
         Rocket.transform.position = new Vector3(-15, 0);
         float x = start;
-        float y = Mathf.Sin(x);
+        float y = Y(x);
         for (int i = 0; i < Points; i++)
         {
             float progress = (float)i / (Points - 1);
             float x_next = Mathf.Lerp(start,end, progress);
-            float y_next = Mathf.Sin(x_next);
-            
+            float y_next = Y(x_next);
+
             Vector3 thisPos = new Vector3(x, y);
             Vector3 nextPos = new Vector3(x_next, y_next);
             Vector3 direction = (nextPos - thisPos).normalized;
@@ -68,5 +113,11 @@ public class CurveController : MonoBehaviour
             yield return new WaitForSeconds(1 / (RocketSpeed * (Points / 3)));
         }
         Rocket.transform.position = new Vector3(15,0);
+        isLaunched = false;
+    }
+
+    private float Y(float x)
+    {
+        return (float)levels[currentLevelIndex].applyLevelFunction(x, a.value, b.value, c.value, d.value);
     }
 }
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -9,31 +9,42 @@ class Level
     public List<Vector2> obstacles { get; }
     public string hint { get; }
     public Difficulty difficulty { get; }
+    public Function function { get; }
 
-    private readonly Func<double, double, double, double, double, double> function;
-    public readonly int parameterCount;
+    private readonly Func<double, double, double, double, double, double> func;
+    public readonly ISet<Parameter> parameters;
 
     public Level(
         Difficulty difficulty, 
         Function function,
         Vector2[] points,
-        Vector2[] obstacles
+        Vector2[] obstacles,
+        params Parameter[] parameters
         )
     {
         this.points = points.ToList();
         this.obstacles = obstacles.ToList();
         this.difficulty = difficulty;
-        this.hint = "No hint.";
+        this.function = function;
         switch(function)
-        { 
-            case Function.Sine: parameterCount = 4; this.function = sine; break;
-            case Function.Linear: parameterCount = 2; this.function = linear; break;
-            case Function.Square: parameterCount = 3; this.function = square; break;
-            case Function.Cubic: parameterCount = 4; this.function = cubic; break;
+        {
+            case Function.Sine: func = sine; break;
+            case Function.Linear: func = linear; break;
+            case Function.Square: func = square; break;
+            case Function.Cubic: func = cubic; break;
         }
+        this.hint = "No hint.";
+        this.parameters = parameters.ToHashSet();
     }
     
-    public Level(Difficulty difficulty, Function function, string hint, Vector2[] points, Vector2[] obstacles) : this(difficulty, function, points, obstacles)
+    public Level(
+        Difficulty difficulty,
+        Function function, 
+        string hint, 
+        Vector2[] points, 
+        Vector2[] obstacles,
+        params Parameter[] parameters
+        ) : this(difficulty, function, points, obstacles, parameters)
     {
         this.hint = hint;
     }
@@ -41,10 +52,10 @@ class Level
     //add levels here
     public static List<Level> levels = new()
     {
-        new Level(Difficulty.Easy, Function.Sine, "All ones.", new Vector2[]{new(-1,-1), new(0,0), new(1,1)}, new Vector2[]{})
+        new Level(Difficulty.Tutorial, Function.Sine, "All ones.", new Vector2[]{new(-1,-1), new(0,0), new(1,1)}, new Vector2[]{}, Parameter.a)
     };
 
-    public double applyLevelFunction(double x, double a, double b, double c, double d) => function(x, a, b, c, d);
+    public double applyLevelFunction(double x, double a, double b, double c, double d) => func(x, a, b, c, d);
     private static double sine(double x, double a, double b, double c, double d) => a * System.Math.Sin(b * x + c) + d;
     private static double linear(double x, double a, double b, double c, double d) => a * x + b;
     private static double square(double x, double a, double b, double c, double d) => a * x * x + linear(x, b, c, c, d);
@@ -59,4 +70,9 @@ public enum Difficulty
 public enum Function
 {
     Sine, Linear, Square, Cubic
+}
+
+public enum Parameter
+{
+    a, b, c, d
 }
